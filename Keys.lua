c12e9541fa850e4b4aecab7a83356392de39f281
DrOxlotus <alex.metz1024@gmail.com>
2018-08-22 22:03:24 -0500

    Added the BfA pre-patch Demon Hunter gear to the DB.

--- Keys.lua
+++ Keys.lua
@@ -1,9 +1,19 @@
 local JunkOrNot, namespace = ...;
 local sell, keep = "|cffFF0000Sell|r", "|cff008000Keep|r";
 local items = {
-   ["Dark Blaze Gauntlets"] = sell,
-   ["The Frost Lord\'s War Cloak"] = sell,
-   ["Foul%-Sadist\'s Seal"] = sell
+  ["Felbound Footwraps"] = keep,
+  ["Felbound Gardbrace"] = keep,
+  ["Felbound Handwraps"] = keep,
+  ["Felbound Leggings"] = keep,
+  ["Felbound Shroud"] = keep,
+  ["Felbound Tunic"] = keep,
+  ["Felbound Waistchain"] = keep,
+  ["Felbound Warglaive"] = keep,
+  ["Sephuz\'s Secret"] = keep,
+  ["The Defiler\'s Lost Vambraces"] = keep,
+  ["Dark Blaze Gauntlets"] = sell,
+  ["Foul%-Sadist\'s Seal"] = sell
+  ["The Frost Lord\'s War Cloak"] = sell,
 };
 
 namespace.items = items;
